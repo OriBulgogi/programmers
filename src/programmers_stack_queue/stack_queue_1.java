b51cde15317dda6afd6070f9e8d789acc6de30c4
상호 이 <hn01184@gmail.com>
2022-01-03 23:20:20 +0900

    01.03

--- src/programmers_stack_queue/stack_queue_1.java
+++ src/programmers_stack_queue/stack_queue_1.java
@@ -2,4 +2,17 @@ package programmers_stack_queue;
 
 public class stack_queue_1 {
 
+	public int[] solution(int[] progresses, int[] speeds) {
+        int[] answer = {};
+        return answer;
+    }
+	
+	public static void main(String[] args) {
+		stack_queue_1 st = new stack_queue_1();
+		int[] progresses= {93, 30, 55};
+		int[] speeds = {1, 30, 5};
+		
+		st.solution(progresses, speeds);
+		
+	}
 }
