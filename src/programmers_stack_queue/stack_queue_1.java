91884f544540216e5af8dc1e5fa5107bfbdb63fa
상호 이 <hn01184@gmail.com>
2022-01-04 22:09:06 +0900

    queue1 sol

--- src/programmers_stack_queue/stack_queue_1.java
+++ src/programmers_stack_queue/stack_queue_1.java
@@ -1,18 +1,63 @@
 package programmers_stack_queue;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Queue;
+import java.util.concurrent.ConcurrentLinkedQueue;
+
 public class stack_queue_1 {
 
 	public int[] solution(int[] progresses, int[] speeds) {
-        int[] answer = {};
-        return answer;
-    }
-	
+		Queue<Integer> deadline = new ConcurrentLinkedQueue<Integer>();;
+		
+
+//		for(int i=0; i<progresses.length; i++) {        	
+//			if(((100-progresses[i]) % speeds[i]) == 0){
+//				deadline.add((100-progresses[i]) / speeds[i]); 
+//			}else {
+//				deadline.add(((100-progresses[i]) / speeds[i])+1) ;
+//			}
+//		}
+		
+		  for(int i = 0; i < progresses.length; i++) {
+	            deadline.add((100-progresses[i])%speeds[i] == 0? (100 - progresses[i]) / speeds[i]
+	                    : (100 - progresses[i]) / speeds[i] + 1);
+	        }
+		List<Integer> answer_list = new ArrayList<Integer>();
+		
+		int pop_value = deadline.poll();
+		int pop_num = 1;
+		
+		while(!deadline.isEmpty()) {
+			int comp_value = deadline.poll();
+			if(pop_value >= comp_value) {
+				pop_num++;
+			}else {
+				answer_list.add(pop_num);
+				pop_num =1;
+				pop_value = comp_value;
+			}
+		}
+		answer_list.add(pop_num);
+		
+
+		int[] answer = new int[answer_list.size()];
+		for(int i =0; i<answer.length; i++ ) {
+			answer[i] = answer_list.get(i);
+		}
+		return answer;
+	}
+
 	public static void main(String[] args) {
 		stack_queue_1 st = new stack_queue_1();
+//		int[] progresses= {95, 90, 99, 99, 80, 99};
+//		int[] speeds = {1, 1, 1, 1, 1, 1};
+//		System.out.println(Arrays.toString(st.solution(progresses, speeds)));
+//		
 		int[] progresses= {93, 30, 55};
 		int[] speeds = {1, 30, 5};
-		
-		st.solution(progresses, speeds);
-		
+		System.out.println(Arrays.toString(st.solution(progresses, speeds)));
+
 	}
 }
