b52da1c627c0d9e93f218581da1028026128857d
상호 이 <hn01184@gmail.com>
2022-01-06 15:39:25 +0900

    queue_2

--- src/programmers_stack_queue/stack_queue_2.java
+++ src/programmers_stack_queue/stack_queue_2.java
@@ -1,5 +1,51 @@
 package programmers_stack_queue;
 
-public class stack_queue_2 {
+import java.util.LinkedList;
+import java.util.Queue;
 
+public class stack_queue_2 {
+	class task_map{
+		int location;
+		int priorities;
+		task_map(int location,int priorities){
+			this.location = location;
+			this.priorities = priorities;
+		}
+	}
+	 public int solution(int[] priorities, int location) {
+	        int answer = 0;
+	        Queue<task_map> queue = new LinkedList<>();
+	        for(int i =0; i<priorities.length; i++) {
+		         queue.add(new task_map(i, priorities[i]));
+	        }
+	        int cnt = 0;
+	        while(!queue.isEmpty()) {
+	        	task_map j = queue.poll();
+	        	 int flag = 0;
+	        	for(task_map comp : queue) {
+	        		if(j.priorities < comp.priorities) {
+	        			flag = 1;
+	        			break;
+	        		}
+	        	}
+	        	if(flag == 1) {
+	        		queue.add(j);	        		
+	        	}else {
+	        		cnt ++;
+	        		if(j.location == location) {
+		        		answer = cnt;
+		        		return answer;
+		        	}
+	        	}
+	        }
+	        return answer;
+	    }
+	 public static void main(String[] args) {
+		 stack_queue_2 st2 = new stack_queue_2();
+		 int[] priorities = {2, 1, 3, 2};
+		 int location = 0;
+		 
+		 System.out.println(st2.solution(priorities, location));
+		
+	}
 }
