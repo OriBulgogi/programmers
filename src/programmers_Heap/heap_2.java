054241288a690289372a6accf7d3b57deff88468
상호 이 <hn01184@gmail.com>
2022-01-17 17:40:51 +0900

    c

--- src/programmers_Heap/heap_2.java
+++ src/programmers_Heap/heap_2.java
@@ -1,6 +1,6 @@
 package programmers_Heap;
 
-import java.util.Collections;
+import java.util.Arrays;
 import java.util.PriorityQueue;
 
 public class heap_2 {
@@ -14,20 +14,40 @@ public class heap_2 {
 		@Override
 		public int compareTo(task o) {
 			// TODO Auto-generated method stub
-			return this.arrival_time - o.arrival_time;
+			return this.task_time - o.task_time;
 		}
 	}
 	public int solution(int[][] jobs) {
         //작업시간만큼 time++ => time에 따라서 우선순위큐에 작업추가
 		int answer = 0;
-		int time =0;
-        PriorityQueue<task> origin_queue = new PriorityQueue<>();
-        PriorityQueue<task> task_queue = new PriorityQueue<>(Collections.reverseOrder());
+		int time =0; //수행되고난 직후의 시간
+		int jobsindex =0; //jobs 배열의 인덱스
+		int count =0; //수행된 요청 갯수
+		Arrays.sort(jobs, (o1,o2) -> o1[0] - o2[0]);
+        PriorityQueue<int[]> origin_queue = new PriorityQueue<>();
+        PriorityQueue<int[]> task_queue = new PriorityQueue<>((o1,o2) -> o1[1] - o2[1]);
+        while(count < jobs.length) {
+			while(jobsindex <jobs.length && jobs[jobsindex][0] <=time) {
+				task_queue.add(jobs[jobsindex++]);
+			}
+			
+			if(task_queue.isEmpty()) {
+				time = jobs[jobsindex][0];
+			}else {
+				int[] temp = task_queue.poll();
+				answer +=temp[1] + time - temp[0];
+				time += temp[1];
+				count++;
+			}
+		}
+
+		return (int) Math.floor(answer / jobs.length);
+        /*
+        for(int[] i : jobs) origin_queue.add(i);
         
-        for(int[] i : jobs) origin_queue.add(new task(i));
         while(!origin_queue.isEmpty()) {
-        	for(int i=origin_queue.peek().arrival_time; i <= time; i++) {
-        		task_queue.add(origin_queue.poll());
+        	for(int i=origin_queue.peek()[0]; i <= time; i++) {
+        		task_queue.add(new task(origin_queue.poll()));
         		if(origin_queue.isEmpty()) break;
         	}
         	
@@ -43,6 +63,7 @@ public class heap_2 {
         }
         
         return answer/jobs.length;
+        */
     }
 	public static void main(String[] args) {
 		heap_2 he2 = new heap_2();
