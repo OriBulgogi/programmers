2d117566bf8d093aa5d85a611190e392827d696b
상호 이 <hn01184@gmail.com>
2022-01-30 00:56:08 +0900

    d

--- src/programmser_DFS_BFS/target_number.java
+++ src/programmser_DFS_BFS/target_number.java
@@ -1,10 +1,26 @@
 package programmser_DFS_BFS;
 
 public class target_number {
+    int answer = 0;
+	
 	   public int solution(int[] numbers, int target) {
-	        int answer = 0;
+	        dfs(numbers, target, 0, 0); 
 	        return answer;
 	    }
-	   target_number ta = new target_number();
-	  
+	   public void dfs(int[] numbers, int target, int depth, int sum) {
+		   if(depth == numbers.length) {
+			   if(target==sum) answer++;
+		   }else {
+			   dfs(numbers, target, depth+1, sum +numbers[depth]);
+			   dfs(numbers, target, depth+1, sum - numbers[depth]);
+		   }
+		   
+	   }
+	   public static void main(String[] args) {
+		   target_number ta = new target_number();
+		   int[] numbers = {4,1 ,2 ,1};
+		   int target = 4;
+		   System.out.println(ta.solution(numbers, target));
+	}
+	   
 }
