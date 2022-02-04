7f60830f37d0d079c929acc358765d04b3cb8f82
상호 이 <hn01184@gmail.com>
2022-02-04 23:40:05 +0900

    network -not sol

--- src/programmser_DFS_BFS/network.java
+++ src/programmser_DFS_BFS/network.java
@@ -1,8 +1,22 @@
 package programmser_DFS_BFS;
 
 public class network {
+	int answer = 0;
     public int solution(int n, int[][] computers) {
-        int answer = 0;
+        dfs(n, computers, 0);
         return answer;
     }
+    public void dfs(int n, int[][]computers, int depth) {
+    	
+    }
+    public static void main(String[] args) {
+		network net = new network();
+		int n= 3;
+		int[][] computers = {
+				{1, 1, 0}, 
+				{1, 1, 0}, 
+				{0, 0, 1}
+				};
+		net.solution(n, computers);
+	}
 }
